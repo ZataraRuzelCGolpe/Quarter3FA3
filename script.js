e9177f3db007449c7b49c0143a24c32e924c98c3
ZataraRuzelCGolpe <zatararuzel.golpe@smc.pshs.edu.ph>
2025-03-01 21:23:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function generateThreeRandomNumbers() {
+    const num1 = Math.ceil(Math.random() * 20);
+    const num2 = Math.ceil(Math.random() * 20);
+    const num3 = Math.ceil(Math.random() * 20);
+    return { num1, num2, num3 };
+}
+
+function displayRandomNumbers() {
+    const numbers = generateThreeRandomNumbers();
+    const outputDiv1 = document.getElementById("randomNumber1");
+    const outputDiv2 = document.getElementById("randomNumber2");
+    const outputDiv3 = document.getElementById("randomNumber3");
+    const biggestDiv = document.getElementById("biggestnum");
+    const letterDiv = document.getElementById("letter"); 
+
+    if (outputDiv1 && outputDiv2 && outputDiv3 && biggestDiv && letterDiv) 
+    { 
+        outputDiv1.textContent = numbers.num1;
+        outputDiv2.textContent = numbers.num2;
+        outputDiv3.textContent = numbers.num3;
+        biggestDiv.textContent = "The missing number for the passcode to unlock the passage door is: " + Math.max(numbers.num1, numbers.num2, numbers.num3);
+
+        const letterIndex = numbers.num1 - 1; 
+        const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const letter = alphabet.charAt(numbers.num1 - 1);
+
+        if (letter) {
+            letterDiv.textContent = "The letter needed in the passcode is: " + letter;
+        } else {
+            letterDiv.textContent = "Invalid letter index.";
+        }
+        const totalMin = numbers.num2 * numbers.num3;
+        const hours = Math.floor(totalMinutes / 60);
+        const minutes = totalMin % 60;
+        timeBreakdownDiv.textContent = "The hr-min breakdown of" + (numbers.num2 * numbers.num3) +"mins is:" + hours + "hours and" + minutes + "minutes";
+    } 
+    else 
+    {
+        if (!outputDiv1 || !outputDiv2 || !outputDiv3) {
+            console.error("One or more elements with id 'randomNumber' not found.");
+        }
+        if (!biggestDiv) {
+            console.error("Element with id 'biggestnum' not found.");
+        }
+        if (!letterDiv) {
+            console.error("Element with id 'letter' not found.");
+        }
+    }
+}
+
+displayRandomNumbers();
